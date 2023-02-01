b4e8ecc2ec48951ec6279c827dd9dc877359614f
gburry-fireblocks <gburry@fireblocks.com>
2023-02-01 16:42:22 -0500

    chore: clean up python modules & setup

--- packages/server/res/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-pip install pyinstaller eth-utils eth-keys bitcoin-utils bip32 bip32utils pynacl flask jinja2 markupsafe waitress toolz coincurve
