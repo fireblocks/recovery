e135eb365ea462d080979490cf48a5252a98419b
gburry-fireblocks <gburry@fireblocks.com>
2023-02-01 20:25:01 -0500

    fix(relay): disable reload on online

--- packages/relay/next.config.js
+++ packages/relay/next.config.js
@@ -3,6 +3,7 @@ const withTM = require("next-transpile-modules")(["shared"]);
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  reloadOnOnline: false,
 });
 
 /**
