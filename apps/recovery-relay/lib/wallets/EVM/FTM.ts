abeced9cd62c9477f442674a2562d5f2d869e385
agoltzman <106151463+agoltzman@users.noreply.github.com>
2023-04-16 13:29:24 +0300

    feat: add Moonbeam wallet

--- apps/recovery-relay/lib/wallets/EVM/FTM.ts
+++ apps/recovery-relay/lib/wallets/EVM/FTM.ts
@@ -4,7 +4,7 @@ import { EVM } from '.';
 export class Fantom extends EVM implements BaseWallet {
   constructor(input: Input) {
     if (input.isTestnet) {
-      throw new Error('No fantom testnet support');
+      throw new Error('No Fantom testnet support');
     }
     super(input, 'https://rpcapi.fantom.network');
   }
