297e139af2aa5728351244e721cfc89a68f75e31
agoltzman <106151463+agoltzman@users.noreply.github.com>
2023-08-06 16:02:43 +0300

    fix: aligned passphrase req to match openssl

--- packages/shared/schemas/generateRsaKeypairInput.ts
+++ packages/shared/schemas/generateRsaKeypairInput.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 import { nonEmptyString } from './scalars';
 
 export const generateRsaKeypairInput = z.object({
-  passphrase: nonEmptyString('Recovery private key passphrase is required').describe('Recovery private key passphrase'),
+  passphrase: nonEmptyString('Recovery private key passphrase is required')
+    .min(4, 'Passphrase must be at least 4 characters')
+    .describe('Recovery private key passphrase'),
 });
 
 export type GenerateRsaKeypairInput = z.infer<typeof generateRsaKeypairInput>;
