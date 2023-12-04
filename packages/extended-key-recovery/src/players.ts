ee03439b48282478e18b55b58808cda526a1d4a4
dainkang1 <dainkang1@gmail.com>
2023-12-04 18:37:11 -0500

    fix: apply yarn linting fixes

--- packages/extended-key-recovery/src/players.ts
+++ packages/extended-key-recovery/src/players.ts
@@ -1,8 +1,7 @@
 import struct from 'python-struct';
 
-export const _uuidToBuffer = (uuid: string): Buffer => {
-  return Buffer.from(uuid.replace('uuid:', '').replace('urn:', '').replace('-', ''), 'hex');
-};
+export const _uuidToBuffer = (uuid: string): Buffer =>
+  Buffer.from(uuid.replace('uuid:', '').replace('urn:', '').replace('-', ''), 'hex');
 
 export const getPlayerId = (keyId: string, deviceId: string, isCloud: boolean): bigint => {
   let playerId;
