f4fb988730b92cbed7a42c0e361e8b982908ca12
Ariel <arielsirota@fireblocks.com>
2023-12-13 16:23:28 +0200

    feat: add pop-up confirm to reaveal priv

--- packages/shared/pages/keys.tsx
+++ packages/shared/pages/keys.tsx
@@ -153,6 +153,9 @@ export const ExtendedKeysBasePage = ({ supportsPrivateKeys, extendedKeys, setExt
                 error={errors.xprv?.message}
                 enableCopy={!!extendedKeys?.xprv}
                 isMonospace
+                confirmRevealRequired={true}
+                confirmMessage='WARNING - You are about to reveal your extended private key (ECDSA).
+                Make sure this machine is OFFLINE and only accessible by authorized personnel.'
                 {...register('xprv')}
               />
             </Grid>
@@ -164,6 +167,9 @@ export const ExtendedKeysBasePage = ({ supportsPrivateKeys, extendedKeys, setExt
                 error={errors.fprv?.message}
                 enableCopy={!!extendedKeys?.fprv}
                 isMonospace
+                confirmRevealRequired={true}
+                confirmMessage='WARNING - You are about to reveal your extended private key (EDDSA).
+                Make sure this machine is OFFLINE and only accessible by authorized personnel.'
                 {...register('fprv')}
               />
             </Grid>
