a3bba18d18197f1eb8854ffbf65d9e1984825c55
agoltzman <106151463+agoltzman@users.noreply.github.com>
2023-12-13 18:31:39 +0200

    chore: :hammer: fix clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,4 @@
 rm -rf node_modules/cache/.turbo
 rm -rf node_modules
-find . -type d -name dist -not -path './node_modules/*' -exec rm -rf {} \;
+find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +
 find . -type d -name .next -exec rm -rf {} \;
-
-yarn install
