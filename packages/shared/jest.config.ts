464d21455e1f21ede0826e8c68147527ab139223
agoltzman <106151463+agoltzman@users.noreply.github.com>
2023-12-17 15:08:17 +0200

    fix: csv macro protection

--- packages/shared/jest.config.ts
+++ packages/shared/jest.config.ts
@@ -2,8 +2,13 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   verbose: true,
-  transform: { '\\.ts$': ['ts-jest', { useESM: true }], '\\.tsx$': ['ts-jest', { useESM: true }] },
+  transform: {
+    '\\.ts$': ['ts-jest', { useESM: true }],
+    '\\.tsx$': ['ts-jest', { useESM: true }],
+    '\\.js$': ['ts-jest', { useESM: true }],
+  },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transformIgnorePatterns: ['../../node_modules/(?!(semver-regex)/)'],
 };
 
 export default config;
