e30e43369cdb585d7666475851e617ed97cc4a20
agoltzman <106151463+agoltzman@users.noreply.github.com>
2024-04-24 12:52:32 +0300

    feat(@fireblocks/recovery-utility): :sparkles: added eth_test6 and new matic testnet

--- apps/recovery-relay/lib/wallets/EVM/ETH.ts
+++ apps/recovery-relay/lib/wallets/EVM/ETH.ts
@@ -5,6 +5,7 @@ import { EVM } from '.';
 export class Ethereum extends EVM implements ConnectedWallet {
   constructor(input: Input) {
     let cluster: string;
+    let rpcEndpoint: string = '';
 
     switch (input.assetId) {
       case 'ETH':
@@ -13,14 +14,19 @@ export class Ethereum extends EVM implements ConnectedWallet {
       case 'ETH_TEST3':
         cluster = 'goerli';
         break;
+      case 'ETH_TEST6':
+        cluster = 'holesky';
+        rpcEndpoint = 'https://ethereum-holesky-rpc.publicnode.com';
+        break;
       case 'ETH_TEST5':
         cluster = 'sepolia';
+        rpcEndpoint = 'https://ethereum-sepolia-rpc.publicnode.com';
         break;
       default:
         throw new Error('Unsupported asset');
     }
 
-    const rpcEndpoint = `https://${cluster}.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`;
+    rpcEndpoint = rpcEndpoint === '' ? `https://${cluster}.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161` : rpcEndpoint;
 
     super(input, rpcEndpoint);
   }
--- apps/recovery-relay/lib/wallets/EVM/MATIC.ts
+++ apps/recovery-relay/lib/wallets/EVM/MATIC.ts
@@ -4,9 +4,21 @@ import { EVM } from '.';
 
 export class Matic extends EVM implements ConnectedWallet {
   constructor(input: Input) {
-    super(
-      input,
-      `${input.isTestnet ? 'https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78' : 'https://polygon-rpc.com/'}`,
-    );
+    let rpcEndpoint: string;
+
+    switch (input.assetId) {
+      case 'MATIC_POLYGON_MUMBAI':
+        rpcEndpoint = 'https://polygon-mumbai-bor-rpc.publicnode.com';
+        break;
+      case 'AMOY_POLYGON_TEST':
+        rpcEndpoint = 'https://polygon-amoy-bor-rpc.publicnode.com';
+        break;
+      case 'MATIC_POLYGON':
+        rpcEndpoint = 'https://polygon-bor-rpc.publicnode.com';
+        break;
+      default:
+        throw new Error('Unsupported asset');
+    }
+    super(input, rpcEndpoint);
   }
 }
