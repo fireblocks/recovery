570df8283c6e3f1a9d42ff026e23c849b729fd83
TomerHFB <158162596+TomerHFB@users.noreply.github.com>
2025-01-09 14:04:39 +0200

    feat(@fireblocks/recovery-utility): :sparkles: add trc20 support

---
+++ apps/recovery-utility/renderer/lib/wallets/TRC20/index.ts
@@ -0,0 +1,40 @@
+import { Tron as BaseTron } from '@fireblocks/wallet-derivation';
+import { SigningWallet } from '../SigningWallet';
+import { GenerateTxInput, TxPayload } from '../types';
+
+export class TRC20 extends BaseTron implements SigningWallet {
+  public async generateTx({ to, amount, feeRate, extraParams }: GenerateTxInput): Promise<TxPayload> {
+    const rpcUrl = extraParams?.get('r');
+
+    // eslint-disable-next-line global-require
+    const TronWeb = require('tronweb');
+    const { HttpProvider } = TronWeb.providers;
+    const fullNode = new HttpProvider(rpcUrl);
+    const solidityNode = new HttpProvider(rpcUrl);
+    const eventServer = new HttpProvider(rpcUrl);
+    const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, this.privateKey?.replace('0x', ''));
+
+    const decimals = extraParams?.get('d');
+    if (!decimals) {
+      this.utilityLogger.error('TRC20: Decimals not set');
+    }
+    const tokenAddress = extraParams?.get('t');
+
+    const functionSelector = 'transfer(address,uint256)';
+    const parameter = [
+      { type: 'address', value: to },
+      { type: 'uint256', value: amount * 10 ** decimals },
+    ];
+
+    const tx = await tronWeb.transactionBuilder.triggerSmartContract(
+      tokenAddress,
+      functionSelector,
+      { feeLimit: feeRate },
+      parameter,
+    );
+
+    const signedTx = await tronWeb.trx.sign(tx.transaction);
+
+    return signedTx;
+  }
+}
