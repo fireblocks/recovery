f5d7fce851e13ee237d742d29027e509d53a4391
agoltzman <106151463+agoltzman@users.noreply.github.com>
2025-04-13 16:24:47 +0300

    test: :white_check_mark: update test to navigate to vault properly

---
+++ .changeset/spotty-planets-jump.md
@@ -0,0 +1,5 @@
+---
+'@fireblocks/e2e-tests': minor
+---
+
+update vault navigation to be able to go to vault from main page or from specific vault
--- packages/e2e-tests/test-utils.ts
+++ packages/e2e-tests/test-utils.ts
@@ -8,6 +8,18 @@ export const navigateToVault = async (page: Page, vaultToNavigateTo: number) => 
     return;
   } catch (e) {
     console.log(`Utility is not in Vault ${vaultToNavigateTo}`, e);
+    try {
+      try {
+        await page.getByRole('link', { name: 'My Vault' }).innerText({ timeout: 5000 });
+        await page.getByRole('link', { name: 'My Vault' }).click();
+      } catch (e) {
+        console.log(`Not in a specifiv vault, willt try to move to one`, e);
+      }
+      await page.getByRole('cell', { name: `Vault ${vaultToNavigateTo}` }).click();
+      return;
+    } catch (e) {
+      console.log(`Failed to navigate to Vault ${vaultToNavigateTo}`, e);
+    }
   }
   await page.getByLabel('Close modal').click();
   await page.getByRole('link', { name: 'My Vault' }).click();
