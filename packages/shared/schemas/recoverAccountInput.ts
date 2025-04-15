67b1d881b550404da887dab62689ef8f97526eb4
agoltzman <106151463+agoltzman@users.noreply.github.com>
2025-04-15 11:21:47 +0300

    feat: :sparkles: add option to select recover specific vault account

--- packages/shared/schemas/recoverAccountInput.ts
+++ packages/shared/schemas/recoverAccountInput.ts
@@ -3,6 +3,23 @@ import { nonEmptyString } from './scalars';
 
 export const recoverAccountInput = z.object({
   name: nonEmptyString('Account name is required').describe('Vault Account name'),
+  id: z.coerce.number().positive().optional(),
 });
+
+export const recoverAccountInputByAccounts = (accountKeys: number[]) =>
+  z
+    .object({
+      name: nonEmptyString('Account name is required').describe('Vault Account name'),
+      id: z.coerce.number().positive().optional(),
+    })
+    .superRefine((data, ctx) => {
+      if ((data.id !== undefined && accountKeys.includes(data.id)) || data.id === undefined) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Account id is already usd, please select another one',
+          path: ['id'],
+        });
+      }
+    });
 
 export type RecoverAccountInput = z.infer<typeof recoverAccountInput>;
